5c7830be5f13fdc8d5c785d28f7ac0af01c76439
bsbilal <bilal530@gmail.com>
2020-11-10 15:58:00 +0300

    api push
    first api commit

---
+++ osts_api/app.js
@@ -0,0 +1,101 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mysql = require('mysql');
+var bodyParser = require('body-parser')
+var fsg = require('fs');
+
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const port = 3006;
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+
+
+
+
+
+// db connection
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '12345678',
+  database : 'osts',
+});
+
+connection.connect(function(err){
+  if(!err) {
+    console.log("Database has connected ...");
+  } else {
+    console.log("Error connecting database ...");
+  }
+});
+
+// hand from other paths
+global.db = connection;
+global.fs=fsg;
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
+
+// CORS
+
+app.all("/*", function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "/*");
+  res.header("Access-Control-Allow-Credentials",true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Accept,X-Access-Token,X-Key,Authorization,X-Requested-With,Origin,Access-Control-Allow-Origin,Access-Control-Allow-Credentials');
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+  } else {
+    next();
+  }
+});
+
+
+module.exports = app;
