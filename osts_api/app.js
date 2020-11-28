0f5e962db61fc18c4d94ebe5e152bc616869de93
bsbilal <bilal530@gmail.com>
2020-11-28 20:55:42 +0300

    lectures list
    lecture lkisting has added

--- osts_api/app.js
+++ osts_api/app.js
@@ -13,6 +13,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var studentRouter = require('./routes/students');
 var homeworkRouter = require('./routes/homeworks');
+var lectureRouter = require('./routes/lecture');
 
 
 var app = express();
@@ -40,6 +41,7 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 app.use('/student', studentRouter);
 app.use('/homework', homeworkRouter);
+app.use('/lecture',lectureRouter)
 
 
 
